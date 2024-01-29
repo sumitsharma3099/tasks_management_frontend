087953e6feb6d7f315a282bed8bc8d58bc87749d
Sumit <ss828863@gmail.com>
2024-01-29 12:19:29 +0530

    setup task management frontend build

--- src/constants/TokenAndRoutes.js
+++ src/constants/TokenAndRoutes.js
@@ -1,6 +1,4 @@
-let token = localStorage.getItem("token");
-let user_full_name = localStorage.getItem("user_full_name");
-const base_url = `http://localhost:3001/`;
+const base_url = `https://tasks-management-backend.onrender.com/`;
 const authorized_url = base_url + `authorized`;
 const login_url = base_url + `login`;
 const new_task_url = base_url + `tasks`;
@@ -10,4 +8,4 @@ const delete_task_url = base_url + `tasks/`;
 const tasks_list_url = base_url + `tasks`;
 const task_edit_url = base_url + `tasks/`;
 
-export { token, user_full_name, authorized_url, login_url, new_task_url, signup_url, task_detail_view_url, delete_task_url, tasks_list_url, task_edit_url }
+export { authorized_url, login_url, new_task_url, signup_url, task_detail_view_url, delete_task_url, tasks_list_url, task_edit_url }
